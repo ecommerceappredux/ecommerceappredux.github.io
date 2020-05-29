48e3e0efb1da9c6bb608502ced84f455fa87c0b5
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2020-05-29 14:06:54 -0700

    'commit'

---
+++ precache-manifest.1db9a5054d4713298be11618d2d8f916.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1eebf6ce6719bfc0c44c8fe7f8ce79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375a1a14eae41b62fa64",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "6452c49a715b0b36d2f6",
+    "url": "/static/css/main.7ca98762.chunk.css"
+  },
+  {
+    "revision": "375a1a14eae41b62fa64",
+    "url": "/static/js/2.32c5aa46.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.32c5aa46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6452c49a715b0b36d2f6",
+    "url": "/static/js/main.05a49dad.chunk.js"
+  },
+  {
+    "revision": "9d9090cfb187749fa1aa",
+    "url": "/static/js/runtime-main.1abdbbcc.js"
+  },
+  {
+    "revision": "e9d9be363c68aa6836d030468c6b19ab",
+    "url": "/static/media/a20.e9d9be36.png"
+  },
+  {
+    "revision": "69e48a733d94798e3cbe279d415642fd",
+    "url": "/static/media/a70.69e48a73.png"
+  },
+  {
+    "revision": "e94e85094ea63591095cb6825791368e",
+    "url": "/static/media/acer.e94e8509.png"
+  },
+  {
+    "revision": "cf566e13fcc97e4796017559298a0da5",
+    "url": "/static/media/addias-shirt.cf566e13.png"
+  },
+  {
+    "revision": "a6b037bd68c3b30896b7eecde7631777",
+    "url": "/static/media/adidas-bag.a6b037bd.png"
+  },
+  {
+    "revision": "3e46c3a1e6c602d60ee330792790c064",
+    "url": "/static/media/adidas-shirt.3e46c3a1.png"
+  },
+  {
+    "revision": "d651da57adb480761e3e3bcdc09e91d6",
+    "url": "/static/media/dell.d651da57.png"
+  },
+  {
+    "revision": "e42d3a2fe966a28bd003bd37e1819493",
+    "url": "/static/media/gucci-bag.e42d3a2f.png"
+  },
+  {
+    "revision": "01e9e174701a5f9cad528b36a0c6bb43",
+    "url": "/static/media/gucci-button-shirt.01e9e174.png"
+  },
+  {
+    "revision": "fc5712b52aa08cf94243e06ec3d78674",
+    "url": "/static/media/gucci-pants.fc5712b5.png"
+  },
+  {
+    "revision": "5f7a5b2ff84f61159dc860415824afd1",
+    "url": "/static/media/gucci-shoes.5f7a5b2f.png"
+  },
+  {
+    "revision": "9af1fa68b575c985a3342748efdaec73",
+    "url": "/static/media/gucci-wallpaper.9af1fa68.jpg"
+  },
+  {
+    "revision": "21347b00e5c91f913edf1535df856f72",
+    "url": "/static/media/ipad-air.21347b00.png"
+  },
+  {
+    "revision": "cf4e164218088f92b428da1178fffdf5",
+    "url": "/static/media/ipad.cf4e1642.png"
+  },
+  {
+    "revision": "6d0fc7a17c776f1b6eae87e0167f4daf",
+    "url": "/static/media/iphone-5s.6d0fc7a1.png"
+  },
+  {
+    "revision": "be133cc1055ac180864a138382ca37b1",
+    "url": "/static/media/iphone-x.be133cc1.png"
+  },
+  {
+    "revision": "02a4f3517ae7a3bce6c5e3f8586f190d",
+    "url": "/static/media/iphone-xs-max.02a4f351.png"
+  },
+  {
+    "revision": "92d155c06f298c2e866f608cc1b756c2",
+    "url": "/static/media/logo.92d155c0.png"
+  },
+  {
+    "revision": "b42526e67e3c5981ab618606c3cdd1f5",
+    "url": "/static/media/mac-wallpaper.b42526e6.png"
+  },
+  {
+    "revision": "2f9a2abb995b07a3493057652c658125",
+    "url": "/static/media/mac.2f9a2abb.png"
+  },
+  {
+    "revision": "5ecb5ffb59ae3170df084275190b49a3",
+    "url": "/static/media/nike-bag.5ecb5ffb.png"
+  },
+  {
+    "revision": "2f2395a7c9095eb8f7904411c2726ad6",
+    "url": "/static/media/nike-jordan.2f2395a7.png"
+  },
+  {
+    "revision": "c63ed93dcefa064805500441b7429b95",
+    "url": "/static/media/nike-wallpaper.c63ed93d.jpg"
+  },
+  {
+    "revision": "3c950581c9b6c9b902a9f6a4d7d7c72f",
+    "url": "/static/media/plus.3c950581.svg"
+  },
+  {
+    "revision": "099533deb282918a299d451355caf8db",
+    "url": "/static/media/polo-shirt.099533de.png"
+  },
+  {
+    "revision": "51ef9befb898ff8af487ec1c2dd5e23b",
+    "url": "/static/media/ps3.51ef9bef.png"
+  },
+  {
+    "revision": "f94da4ca40b695551ed7d71ddef74ed3",
+    "url": "/static/media/ps4-wallpaper.f94da4ca.png"
+  },
+  {
+    "revision": "70ced350618780e1289aff01242184f5",
+    "url": "/static/media/ps4.70ced350.png"
+  },
+  {
+    "revision": "b60aed0758fef233068664a43de91ee0",
+    "url": "/static/media/purse.b60aed07.png"
+  },
+  {
+    "revision": "f0684c0a6ec879538f28416e46740c38",
+    "url": "/static/media/shirt.f0684c0a.png"
+  },
+  {
+    "revision": "f2fdabb0f019cba565d679edd3ae9937",
+    "url": "/static/media/shoe.f2fdabb0.png"
+  },
+  {
+    "revision": "cd04c6af81ecfd2044efbf2ae6c40801",
+    "url": "/static/media/tab-s.cd04c6af.png"
+  },
+  {
+    "revision": "de401016a680c000051ec8b6238e3ea3",
+    "url": "/static/media/xbox-360.de401016.png"
+  },
+  {
+    "revision": "02a046b177bd1ddb5bf405b9eefcee83",
+    "url": "/static/media/xbox-one-x.02a046b1.png"
+  },
+  {
+    "revision": "03ecdf5ff9c65b2f7cbca62fa287f156",
+    "url": "/static/media/xbox-wallpaper.03ecdf5f.png"
+  },
+  {
+    "revision": "2ca113234f7a651646b8fbd44116406c",
+    "url": "/static/media/xbox.2ca11323.png"
+  },
+  {
+    "revision": "a60df94eb4f56f75cef051118026924d",
+    "url": "/static/media/yeezy.a60df94e.png"
+  }
+]);
